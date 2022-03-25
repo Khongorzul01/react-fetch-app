a4c2caeeea3d38e193b3bb4bacd7ab0d655d32db
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-03-25 15:45:06 +0800

    update5

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import SearchForm from "./components/SearchForm";
 import GifList from "./components/GifList";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gifs: [],
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "http://api.giphy.com/v1/gifs/trending?api_key=d6LPIdAeDq2hKSjtR8SgQAzSbDzyGU51"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ gifs: data.data });
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }
   render() {
+    console.log(this.state.gifs);
     return (
       <div className="main-header">
         <div className="inner">
@@ -12,7 +31,7 @@ class App extends Component {
           <SearchForm />
         </div>
         <div className="main-content">
-          <GifList />
+          <GifList data={this.state.gifs} />
         </div>
       </div>
     );
