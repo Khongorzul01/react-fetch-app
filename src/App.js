256b78a57667c30931b0e96c537d06e3a83120de
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-03-25 16:08:18 +0800

    update6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { Component } from "react";
 import SearchForm from "./components/SearchForm";
 import GifList from "./components/GifList";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
@@ -11,16 +12,30 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    fetch(
-      "http://api.giphy.com/v1/gifs/trending?api_key=d6LPIdAeDq2hKSjtR8SgQAzSbDzyGU51"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ gifs: data.data });
+    //promise butsaana
+    axios
+      .get(
+        "http://api.giphy.com/v1/gifs/trending?api_key=d6LPIdAeDq2hKSjtR8SgQAzSbDzyGU51"
+      )
+      .then((response) => {
+        this.setState({
+          gifs: response.data.data,
+        });
       })
       .catch((error) => {
         console.log("error", error);
       });
+
+    // fetch(
+    //   "http://api.giphy.com/v1/gifs/trending?api_key=d6LPIdAeDq2hKSjtR8SgQAzSbDzyGU51"
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     this.setState({ gifs: data.data });
+    //   })
+    //   .catch((error) => {
+    //     console.log("error", error);
+    //   });
   }
   render() {
     console.log(this.state.gifs);
