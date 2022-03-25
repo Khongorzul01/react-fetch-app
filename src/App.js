46c933ca7a3e72fe8ceaee80feade72e9cc064c8
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-03-25 16:35:25 +0800

    update7

--- src/App.js
+++ src/App.js
@@ -11,11 +11,25 @@ class App extends Component {
       gifs: [],
     };
   }
-  componentDidMount() {
-    //promise butsaana
+  // componentDidMount() {
+  // const cats = "cats";
+  //promise butsaana
+
+  // fetch(
+  //   "http://api.giphy.com/v1/gifs/trending?api_key=d6LPIdAeDq2hKSjtR8SgQAzSbDzyGU51"
+  // )
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     this.setState({ gifs: data.data });
+  //   })
+  //   .catch((error) => {
+  //     console.log("error", error);
+  //   });
+  // }
+  performSearch = (query) => {
     axios
       .get(
-        "http://api.giphy.com/v1/gifs/trending?api_key=d6LPIdAeDq2hKSjtR8SgQAzSbDzyGU51"
+        `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=d6LPIdAeDq2hKSjtR8SgQAzSbDzyGU51`
       )
       .then((response) => {
         this.setState({
@@ -25,25 +39,14 @@ class App extends Component {
       .catch((error) => {
         console.log("error", error);
       });
-
-    // fetch(
-    //   "http://api.giphy.com/v1/gifs/trending?api_key=d6LPIdAeDq2hKSjtR8SgQAzSbDzyGU51"
-    // )
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     this.setState({ gifs: data.data });
-    //   })
-    //   .catch((error) => {
-    //     console.log("error", error);
-    //   });
-  }
+  };
   render() {
     console.log(this.state.gifs);
     return (
       <div className="main-header">
         <div className="inner">
           <h1 className="main-title">Hello</h1>
-          <SearchForm />
+          <SearchForm onSearch={this.performSearch} />
         </div>
         <div className="main-content">
           <GifList data={this.state.gifs} />
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 import GifList from "./GifList";
 
 export default class SearchForm extends Component {
+  state = {
+    searchText: "",
+  };
+  onSearchChange = (e) => {
+    this.props.onSearch(this.state.searchText);
+    this.setState({ searchText: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    e.currentTarget.reset();
+  };
   render() {
     return (
       <div>
-        <form className="search-form">
+        <form className="search-form" onSubmit={this.handleSubmit}>
           <label className="is-hidden" htmlFor="search">
             Search
           </label>
-          <input name="search" type="search" placeholder="Search...."></input>
+          <input
+            name="search"
+            type="search"
+            placeholder="Search...."
+            onChange={this.onSearchChange}
+          ></input>
           <button type="submit" id="submit" className="search-button">
             <i className="material-icons icn-search"></i>
           </button>
