13b7a1cb906085891ac08b65529d2d74999b4d30
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-03-25 14:46:40 +0800

    update4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { Component } from "react";
 import SearchForm from "./components/SearchForm";
+import GifList from "./components/GifList";
 
 class App extends Component {
   render() {
@@ -9,6 +10,9 @@ class App extends Component {
         <div className="inner">
           <h1 className="main-title">Hello</h1>
           <SearchForm />
+        </div>
+        <div className="main-content">
+          <GifList />
         </div>
       </div>
     );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import GifList from "./GifList";
 
 export default class SearchForm extends Component {
   render() {
@@ -8,7 +9,10 @@ export default class SearchForm extends Component {
           <label className="is-hidden" htmlFor="search">
             Search
           </label>
-          <input placeholder="Search...."></input>
+          <input name="search" type="search" placeholder="Search...."></input>
+          <button type="submit" id="submit" className="search-button">
+            <i className="material-icons icn-search"></i>
+          </button>
         </form>
       </div>
     );
